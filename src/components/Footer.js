a74913f3519493113e0f89ec6de56a8c601d4b26
Siddhi Mule <mulesiddhi@gmail.com>
2021-05-01 21:42:07 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,7 +8,7 @@ const Footer = () => {
         <div className='footer' id='contact' >
             <div className='fbox1'>
             <h1>DataZen</h1>
-            <h3>Strive for greatness.</h3>
+            <h3>Official Council for Data Science</h3>
             <ul classname='ilist'>
                 <li>
                 <Link to={{ pathname: "https://www.instagram.com/" }} target="_blank" className='flinks' >
@@ -16,12 +16,12 @@ const Footer = () => {
                 </Link>
                 </li>
                 <li>
-                <Link to={{ pathname: "https://www.linkedin.com/" }} target="_blank" className='flinks' >
+                <Link to={{ pathname: "https://www.linkedin.com/company/datazen-somaiya" }} target="_blank" className='flinks' >
                 <i class="fab fa-linkedin-in fa-2x"></i>
                 </Link>
                 </li>
                 <li>
-                <Link to={{ pathname: "https://discord.com/" }} target="_blank" className='flinks' >
+                <Link to={{ pathname: "https://discord.gg/tEfkPvDmBN" }} target="_blank" className='flinks' >
                 <i class="fab fa-discord fa-2x"></i>
                 </Link>
                 </li>
@@ -30,7 +30,11 @@ const Footer = () => {
             <div className='fbox2'>
             <h2>Contact Us</h2>
             <div className='address'>
-            <i class="fas fa-map-marker-alt fa-2x"></i>
+            <h3><i class="fas fa-map-marker-alt "></i>  KJ Somaiya College of Engineering </h3>
+            <h3>Vidyanagar, Vidyavihar Mumbai, Maharashtra-400089</h3>
+            </div>
+            <div>
+                <h3><i class="fas fa-envelope"></i>  datazen@somaiya.edu</h3>
             </div>
             </div>
         </div>
