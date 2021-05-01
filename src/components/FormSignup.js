5a07f602c17f1882d2ce3c67fa99057f71211444
Siddhi Mule <mulesiddhi@gmail.com>
2021-05-01 23:30:17 +0530

    changes

--- src/components/FormSignup.js
+++ src/components/FormSignup.js
@@ -1,12 +1,32 @@
-import { Link } from 'react-router-dom'
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
-const FormSignup = () => {
+const FormSignup = (props) => {
 
-    
+    const[user,setUser]=React.useState('');
+    const[userErr,setUserErr]=React.useState({})
+    let history = useHistory();
+
     const handleSubmit=(e)=>{
         e.preventDefault();
-        
+        const isValid=formValidation();
+        if(isValid){
+            history.push('/');
+        }
+    }
+    const formValidation=()=>{
+       const userErr={};
+        let isValid=true;
+        if(user.trim().length<5){
+            userErr.userShort='UserName is too short';
+            isValid=false;
+        }
+        if(!user.includes('123')){
+            userErr.user123='username must have 123';
+            isValid=false;
+        }
+        setUserErr(userErr);
+        return isValid;
     }
 
     return (
@@ -20,16 +40,21 @@ const FormSignup = () => {
                 <div className='form-input'>
                     <label htmlFor='username' className='form-label'> UserName</label>
                         <input id='username' type='text'  name='username' className='form-input' placeholder='Enter Your UserName'
-                        />
+                        required onChange={(e)=>{setUser(e.target.value)}}/>
                 </div>
+                {Object.keys(userErr).map((key)=>{
+                    return <div style={{color:'red'}}>{userErr[key]}</div>
+                }
+                )}
+                <br/>
                 <div className='form-input'>
                     <label htmlFor='email' className='form-label'>Email</label>
-                        <input  id='email' type='email' name='email' className='form-input' placeholder='Enter Your Email'
+                        <input  id='email' type='email' name='email' className='form-input' placeholder='Enter Your Email' required
                        />
                 </div>
                 <div className='form-input password'>
                     <label htmlFor='password' className='form-label'>Password</label>
-                        <input  id='password' type='password' name='password' className='form-input' placeholder='Enter Password'
+                        <input  id='password' type='password' name='password' className='form-input' placeholder='Enter Password' required
                        />
                 </div>
                 <button className='form-input-btn'
