3f186e912148dc5908556be76c9ff92aa6463673
Siddhi Mule <mulesiddhi@gmail.com>
2021-05-01 20:54:28 +0530

    changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import './Footer.css'
 
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 const Footer = () => {
@@ -9,13 +10,19 @@ const Footer = () => {
             <h3>Strive for greatness.</h3>
             <ul classname='ilist'>
                 <li>
+                <Link to={{ pathname: "https://www.instagram.com/" }} target="_blank" className='flinks' >
                 <i class="fab fa-instagram fa-2x"></i> 
+                </Link>
                 </li>
                 <li>
+                <Link to={{ pathname: "https://www.linkedin.com/" }} target="_blank" className='flinks' >
                 <i class="fab fa-linkedin-in fa-2x"></i>
+                </Link>
                 </li>
                 <li>
+                <Link to={{ pathname: "https://discord.com/" }} target="_blank" className='flinks' >
                 <i class="fab fa-discord fa-2x"></i>
+                </Link>
                 </li>
             </ul>
             
