eaeba99b726418af1f79154573143f1165668d90
hetulmehta <hetulmehta08@gmail.com>
2021-06-21 11:56:47 +0530

    team

--- src/components/team/Teamwrap.js
+++ src/components/team/Teamwrap.js
@@ -1,11 +1,285 @@
-import './Teamwrap.css'
+import "./Teamwrap.css";
+import React from "react";
+import { Carousel, Card, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import React from 'react'
+const Team = () => {
+  return (
+    <>
+      <Carousel>
+        <Carousel.Item>
+          <Card style={{ width: "18rem",height: "20rem", marginTop: "300px" }}>
+            <Card.Img variant="top" src="https://mdbcdn.b-cdn.net/img/new/standard/nature/111.jpg"  />
+            <Card.Body>
+              <Card.Title style={{position: "absolute", left: "100px"}}>Jane Doe</Card.Title>
+              <Card.Text  style={{position: "absolute", top: "250px", left: "100px"}}>
+               Co-Founder
+              </Card.Text>
+            </Card.Body>
+          </Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "300px",
+              position: "absolute",
+              top: "0px",
+              left: "620px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "300px",
+              position: "absolute",
+              top: "0px",
+              left: "935px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "300px",
+              position: "absolute",
+              top: "0px",
+              left: "1250px",
+            }}
+          ></Card>
+        </Carousel.Item>
+      </Carousel>
+      <Carousel>
+        <Carousel.Item>
+          <Card
+            style={{ width: "18rem", marginTop: "170px", marginLeft: "150px" }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "770px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "1088px",
+            }}
+          ></Card>
+        </Carousel.Item>
+      </Carousel>
+      <Carousel>
+        <Carousel.Item>
+          <Card style={{ width: "18rem", marginTop: "170px" }}></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "620px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "935px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "1250px",
+            }}
+          ></Card>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Card
+            style={{ width: "18rem", marginTop: "170px", marginLeft: "150px" }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "770px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "1088px",
+            }}
+          ></Card>
+        </Carousel.Item>
+      </Carousel>
+      <Carousel>
+        <Carousel.Item>
+          <Card style={{ width: "18rem", marginTop: "170px" }}></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "620px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "935px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "1250px",
+            }}
+          ></Card>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Card style={{ width: "18rem", marginTop: "170px" }}></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "620px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "935px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "1250px",
+            }}
+          ></Card>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Card style={{ width: "18rem", marginTop: "170px" }}></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "620px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "935px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "1250px",
+            }}
+          ></Card>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Card style={{ width: "18rem", marginTop: "170px" }}></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "620px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "935px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "1250px",
+            }}
+          ></Card>
+        </Carousel.Item>
+        <Carousel.Item>
+          <Card style={{ width: "18rem", marginTop: "170px" }}></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "620px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "935px",
+            }}
+          ></Card>
+          <Card
+            style={{
+              width: "18rem",
+              marginTop: "170px",
+              position: "absolute",
+              top: "0px",
+              left: "1250px",
+            }}
+          ></Card>
+        </Carousel.Item>
+      </Carousel>
+    </>
+  );
+};
 
-const Teams = () => {
-    return (
-     <div></div>
-    )
-}
-
-export default Teams;
+export default Team;
