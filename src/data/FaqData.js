f92cb2efb10e31ce203e5025fdb53dbfb30418b6
Hinal Pujara <hinal.pujara@somaiya.edu>
2021-07-02 16:40:49 +0530

    Updated FaqData.js

--- src/data/FaqData.js
+++ src/data/FaqData.js
@@ -1,33 +1,33 @@
 const Data = [
     {
       id: 1,
-      title: 'Do I have to allow the use of cookies?',
+      title: 'What is DataZen ?',
       info:
-        'Unicorn vinyl poutine brooklyn, next level direct trade iceland. Shaman copper mug church-key coloring book, whatever poutine normcore fixie cred kickstarter post-ironic street art.',
+        'DataZen is the official Data Science council of Somaiya Vidyavihar University.',
     },
     {
       id: 2,
-      title: 'How do I change my My Page password?',
+      title: 'What do we do at DataZen ?',
       info:
-        'Coloring book forage photo booth gentrify lumbersexual. Migas chillwave poutine synth shoreditch, enamel pin thundercats fashion axe roof party polaroid chartreuse.',
+        'Here, at Datazen it is our aim to help people learn and understand Data and tasks pertaining to it. This community is here to enhance the understanding, knowledge and experience of analysing data, taking it step by step, talking to people from around the world and understand from every perspective and every field. It is a place that gives students the platform to think out of the box and become as creativeas they can get with data and create something from the very scratch.'
     },
     {
       id: 3,
-      title: 'What is BankID?',
+      title: 'What activities do we organise ?',
       info:
-        'Enamel pin fam sustainable woke whatever venmo. Authentic asymmetrical put a bird on it, lumbersexual activated charcoal kinfolk banjo cred pickled sartorial.',
+        'We organise activities that help the youth grow, have speaker sessions to gain current world knowledge and finally competitions to check ones progress and lastly a discord community that\'s available for everyone at all times to post their queries or achievements or just to interact with people who have the same interest as yours.',
     },
     {
       id: 4,
-      title: 'Whose birth number can I use?',
+      title: 'Can I join team DataZen even if idk coding ?',
       info:
-        'Edison bulb direct trade gentrify beard lo-fi seitan sustainable roof party franzen occupy squid. Knausgaard cronut succulents, scenester readymade shabby chic lyft. Copper mug meh vegan gentrify.',
+        'Yes, 21 days provides you with a platform to explore and learn and grow. Like the only thing you need is the will to learn and grow And there\'s a platform where material is provided along with partner if you want to do it with.',
     },
     {
       id: 5,
-      title: 'When do I recieve a password ordered by letter?',
+      title: 'Does data science have a future ?',
       info:
-        'Locavore franzen fashion axe live-edge neutra irony synth af tilde shabby chic man braid chillwave waistcoat copper mug messenger bag. Banjo snackwave blog, microdosing thundercats migas vaporware viral lo-fi seitan ',
+        'There is already increasing automation in data science, and it is certainly not going to slow down. You can already program a machine based on multiple parameters to find the best possible business solution to a problem. In the past statisticians had to manually design their predictive models and readjust them over and over again. Growing amounts of data and ever more complex business problems make this very difficult without applying the concepts of data science. In this context, data science is here to stay for a very long time. ',
     },
   ]
   export default Data;
