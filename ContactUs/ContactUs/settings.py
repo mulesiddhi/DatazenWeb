6644780f3fa140abcf0e757d188d0ee4996c04f8
nishant <n.rajadhyaksha@somaiya.edu>
2021-07-02 23:53:13 +0530

    requirements.txt

--- ContactUs/ContactUs/settings.py
+++ ContactUs/ContactUs/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x+2r=k9oux#6cw4cuntbnmsm@(6(=i=(om%1!x#h!*u6ei2*s@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
