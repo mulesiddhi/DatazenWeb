37bde33effe981800613ac6876876460f67e5730
Siddhi Mule <mulesiddhi@gmail.com>
2021-07-03 16:19:53 +0530

    about us changes

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,10 @@ const About = () => {
             <div className='cards_Container bg-white'>
                 <div className='cards_Wrapper bg-white'>
                   <div className='box1 bg-white' >
-                  <p className='bg-white'>DataZen, the official Data Science council of Somaiya Vidyavihar University and we want to help everyone understand and work with data irrespective of their current current knowledge and field. </p>
+                  <p className='bg-white'>We are the official data science council of Somaiya Vidyavihar University and we want to help everyone understand and work with data irrespective of their current knowledge and field. </p>
                   
 
-<p className='bg-white'>We organize activities that help the youth grow, have speaker sessions to gain current world knowledge and finally competitions to check one's progress and lastly a discord community that's available for everyone at all times to post their queries or achievements or just to interact with people who have the same interest as yours.</p>
+<p className='bg-white'>We organize activities that help the youth grow, have speaker sessions to gain current world knowledge and finally competitions to check one's progress and lastly a discord community that's available for everyone at all times to post their queries or achievements or just to interact with people who have the same interest as yours..</p>
                       </div>  
                 </div>
             </div>
